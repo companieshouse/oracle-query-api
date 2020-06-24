415763d10f6bab0238177e7c601de4cc05e439fb
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-06-24 09:08:08 +0100

    Remove unused import

--- src/main/java/uk/gov/ch/service/emergencyauthcode/impl/EmergencyOfficersServiceImpl.java
+++ src/main/java/uk/gov/ch/service/emergencyauthcode/impl/EmergencyOfficersServiceImpl.java
@@ -2,7 +2,6 @@ package uk.gov.ch.service.emergencyauthcode.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import uk.gov.ch.OracleQueryApplication;
