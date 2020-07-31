ec3fe68bcc3e627323c1dfd761ced5266a4ce192
rsachdevch <rsachdev@companieshouse.gov.uk>
2020-07-31 11:04:13 +0100

    Replace catch all with individual imports

--- src/test/java/uk/gov/ch/service/emergencyauthcode/impl/EmergencyOfficersServiceImplTest.java
+++ src/test/java/uk/gov/ch/service/emergencyauthcode/impl/EmergencyOfficersServiceImplTest.java
@@ -1,6 +1,9 @@
 package uk.gov.ch.service.emergencyauthcode.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
