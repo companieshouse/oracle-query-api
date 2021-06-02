03202ae4e3b0d8fe02b251a8c2e2280b1c8e109e
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-06-02 12:24:00 +0100

    Remove unused imports.

--- src/test/java/uk/gov/ch/dao/ShareholderDaoTest.java
+++ src/test/java/uk/gov/ch/dao/ShareholderDaoTest.java
@@ -4,10 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
