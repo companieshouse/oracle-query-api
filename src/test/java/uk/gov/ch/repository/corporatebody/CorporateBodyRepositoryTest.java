2003b0bcec0edf3c7b10e2468f2bd920ee0d56bb
svarallo <svarallo@companieshouse.gov.uk>
2021-06-02 15:36:48 +0100

    ncs-243-removing-code-smell

--- src/test/java/uk/gov/ch/repository/corporatebody/CorporateBodyRepositoryTest.java
+++ src/test/java/uk/gov/ch/repository/corporatebody/CorporateBodyRepositoryTest.java
@@ -19,7 +19,7 @@ import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class CorporateBodyRepositoryTest {
+class CorporateBodyRepositoryTest {
 
     @Mock
     private JdbcTemplate jdbcTemplate;
