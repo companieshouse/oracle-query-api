0a0e1858ffdcb61beba1929fdfa48d2e6ff44be7
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-08-23 13:29:58 +0100

    ncs-632 Added json property

--- src/main/java/uk/gov/ch/model/payment/ConfirmationStatementPaymentJson.java
+++ src/main/java/uk/gov/ch/model/payment/ConfirmationStatementPaymentJson.java
@@ -1,7 +1,10 @@
 package uk.gov.ch.model.payment;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ConfirmationStatementPaymentJson {
 
+    @JsonProperty("paid")
     private Boolean isPaid;
 
     public Boolean isPaid() {
