1e903396eab9f40cd8a77f3bf53459e1986a0c89
Matthew Culpin <mculpin@companieshouse.gov.uk>
2021-09-07 13:27:24 +0100

    NCS-661 Update payment query from date to period of time

--- src/main/java/uk/gov/ch/repository/payment/ConfirmationStatementPaymentCheckRepository.java
+++ src/main/java/uk/gov/ch/repository/payment/ConfirmationStatementPaymentCheckRepository.java
@@ -13,6 +13,8 @@ public interface ConfirmationStatementPaymentCheckRepository extends CrudReposit
            "CSPP.paid_by_transaction_id " +
            "from CORPORATE_BODY CB " +
            "inner join CONFIRMATION_STMT_PYMT_PERIOD CSPP on CB.CORPORATE_BODY_ID=CSPP.CORPORATE_BODY_ID " +
-           "where CB.INCORPORATION_NUMBER=?1 and CSPP.PAYMENT_PERIOD_DUE_DATE=?2", nativeQuery = true)
+           "where CSPP.PAYMENT_PERIOD_DUE_DATE >= TO_DATE (?2, 'YYYY-MM-dd') " +
+           "and CSPP.PAYMENT_PERIOD_START_DATE <= TO_DATE (?2, 'YYYY-MM-dd') " +
+           "and CB.INCORPORATION_NUMBER=?1", nativeQuery = true)
     Optional<ConfirmationStatementPayment> findPaymentsForPeriod(String companyNumber, Date dueDate);
 }
