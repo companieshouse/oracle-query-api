9e1b3964b44c3a3a3e0b70b3104460ef37a2d9c0
Lewis Evans <levans@companieshouse.gov.uk>
2021-09-07 10:06:28 +0100

    change id column to row number to allow no register companies with sail address through

--- src/main/java/uk/gov/ch/repository/register/RegisterLocationRepository.java
+++ src/main/java/uk/gov/ch/repository/register/RegisterLocationRepository.java
@@ -18,29 +18,15 @@ public interface RegisterLocationRepository extends PagingAndSortingRepository<R
             + " ad.POST_CODE AS SAIL_ADDRESS_POST_CODE,"
             + " ad.REGION AS SAIL_ADDRESS_REGION,"
             + " rt.register_type_desc,"
-            + " rt.register_type_id"
+            + " ROW_NUMBER() OVER (ORDER BY (ad.address_id)) AS UNIQUE_ID"
             + " FROM corporate_body cb"
             + " INNER JOIN corbod_address_link cbal on cbal.corporate_body_id=cb.corporate_body_id"
             + " INNER JOIN address ad on ad.address_id=cbal.address_id"
             + " INNER JOIN corporate_body_address_type cbat on cbat.address_type_id=cbal.address_type_id"
-            + " INNER JOIN corbod_regtyp_link cbrl on cbrl.corporate_body_id=cb.corporate_body_id"
-            + " INNER JOIN register_type rt on rt.register_type_id=cbrl.register_type_id"
-            + " INNER JOIN register_link_type rlt on rlt.register_link_type_id=cbrl.register_link_type_id"
+            + " LEFT JOIN corbod_regtyp_link cbrl on cbrl.corporate_body_id=cb.corporate_body_id"
+            + " FULL OUTER JOIN register_type rt on rt.register_type_id=cbrl.register_type_id"
             + " WHERE cb.incorporation_number = ?"
-            + " AND cbat.address_type_id = 5000"
-            + " AND rlt.register_link_type_id = 1"
-            + " GROUP BY"
-            + " rt.register_type_id,"
-            + " cbat.address_type_desc,"
-            + " ad.HOUSE_NAME_NUMBER,"
-            + " ad.STREET, ad.CARE_OF,"
-            + " ad.COUNTRY_NAME,"
-            + " ad.POST_TOWN,"
-            + " ad.PO_BOX,"
-            + " ad.POST_CODE,"
-            + " ad.REGION,"
-            + " rt.register_type_desc,"
-            + " rlt.register_link_type_desc", nativeQuery = true)
+            + " AND cbat.address_type_id = 5000", nativeQuery = true)
     Page<RegisterLocation> getRegisterLocation(String incorporationNumber, Pageable pageable);
 
 }
