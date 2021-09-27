ae51f3b3683374a795673324c6bcd627db7a7907
Alfred Bwomezi <ABwomezi@companieshouse.gov.uk>
2021-09-27 12:16:51 +0100

    Fix code smells.

--- src/main/java/uk/gov/ch/util/CustomTextFormat.java
+++ src/main/java/uk/gov/ch/util/CustomTextFormat.java
@@ -4,6 +4,8 @@ import org.codehaus.plexus.util.StringUtils;
 
 public class CustomTextFormat {
 
+    private CustomTextFormat() {}
+
     /**
      * Formats the text case to a standard sentence formatting. First letter uppercase while the rest lowercase.
      * @param text The text to be formatted.
@@ -12,10 +14,10 @@ public class CustomTextFormat {
     public static String formatStandardSentence(String text) {
         if(text == null || text.isEmpty()) return text;
 
-        String formatted = "";
+        StringBuilder stringBuilder = new StringBuilder();
         for (String sub : text.toLowerCase().split("\\. ")) {
-            formatted = formatted + ". " + StringUtils.capitalizeFirstLetter(sub) ;
+            stringBuilder.append(". ").append(StringUtils.capitalizeFirstLetter(sub));
         }
-        return formatted.substring(2).trim();
+        return stringBuilder.delete(0,1).toString().trim();
     }
 }
