5fb87784ef8e14f51c9f135d24d7ae0559d1d270
Lewis Evans <levans@companieshouse.gov.uk>
2021-11-16 14:02:53 +0000

    bi-9482 retrieve officer type details

--- src/main/java/uk/gov/ch/model/officer/active/ActiveOfficerDetails.java
+++ src/main/java/uk/gov/ch/model/officer/active/ActiveOfficerDetails.java
@@ -72,6 +72,26 @@ public class ActiveOfficerDetails {
     private String residentialAddressRegion;
     @Column(name = "SECURE_INDICATOR")
     private String secureIndicator;
+    @Column(name = "corporate")
+    @JsonProperty("is_corporate")
+    private boolean corporate;
+    @Column(name = "role")
+    private String role;
+    @Column(name = "place_registered")
+    @JsonProperty("place_registered")
+    private String placeRegistered;
+    @Column(name = "registration_number")
+    @JsonProperty("registration_number")
+    private String registrationNumber;
+    @Column(name = "law_governed")
+    @JsonProperty("law_governed")
+    private String lawGoverned;
+    @Column(name = "legal_form")
+    @JsonProperty("legal_form")
+    private String legalForm;
+    @Column(name = "identification_type")
+    @JsonProperty("identification_type")
+    private String identificationType;
 
     @Transient
     @JsonProperty("service_address")
@@ -219,6 +239,62 @@ public class ActiveOfficerDetails {
 
     public void setDateOfAppointment(String dateOfAppointment) {
         this.dateOfAppointment = dateOfAppointment;
+    }
+
+    public void setCorporate(boolean corporate) {
+        this.corporate = corporate;
+    }
+
+    public boolean isCorporate() {
+        return corporate;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getPlaceRegistered() {
+        return placeRegistered;
+    }
+
+    public void setPlaceRegistered(String placeRegistered) {
+        this.placeRegistered = placeRegistered;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getLawGoverned() {
+        return lawGoverned;
+    }
+
+    public void setLawGoverned(String lawGoverned) {
+        this.lawGoverned = lawGoverned;
+    }
+
+    public String getLegalForm() {
+        return legalForm;
+    }
+
+    public void setLegalForm(String legalForm) {
+        this.legalForm = legalForm;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
     }
 
     public Address getServiceAddress() {
