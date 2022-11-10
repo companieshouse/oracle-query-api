bdda8e01124131fcdd6f8d1e534e1582bd7e7c8f
dsanson <dsanson@companieshouse.gov.uk>
2022-11-10 17:26:14 +0000

    add case type field to search entity and results to enable field to show on results table

--- src/main/java/uk/gov/ch/model/officer/bankrupt/ScottishBankruptOfficerSearchResult.java
+++ src/main/java/uk/gov/ch/model/officer/bankrupt/ScottishBankruptOfficerSearchResult.java
@@ -20,6 +20,7 @@ public class ScottishBankruptOfficerSearchResult {
     private String postcode;
     private LocalDate dateOfBirth;
     private LocalDate debtorDischargeDate;
+    private String CaseType;
 
 
     public String getEphemeralKey() {
@@ -118,5 +119,13 @@ public class ScottishBankruptOfficerSearchResult {
 
     public void setDebtorDischargeDate(LocalDate debtorDischargeDate) {
         this.debtorDischargeDate = debtorDischargeDate;
+    }
+
+    public String getCaseType() {
+        return CaseType;
+    }
+
+    public void setCaseType(String caseType) {
+        CaseType = caseType;
     }
 }
