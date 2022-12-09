5180422f3bc9bd41960844f2aa4eb91e8e2045a9
adr1ancho <adrian.cho.j@gmail.com>
2022-12-09 15:07:24 +0000

    IE-130 Address Sonar issues in oracle-query-api

--- src/main/java/uk/gov/ch/transformers/officer/bankrupt/BankruptOfficersTransformer.java
+++ src/main/java/uk/gov/ch/transformers/officer/bankrupt/BankruptOfficersTransformer.java
@@ -1,16 +1,16 @@
 package uk.gov.ch.transformers.officer.bankrupt;
 
 import java.time.LocalDate;
-import org.springframework.data.domain.Page;
-
-import org.springframework.stereotype.Component;
-import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDetails;
-import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDataModel;
-import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearchResult;
-import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearchResults;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+
+import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDataModel;
+import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDetails;
+import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearchResult;
+import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearchResults;
 
 @Component
 public class BankruptOfficersTransformer {
@@ -63,29 +63,30 @@ public class BankruptOfficersTransformer {
 
         return details;
     }
-    public ScottishBankruptOfficerSearchResult convertToSearchResult(ScottishBankruptOfficerDataModel scottishBankruptOfficerDetailsDataModel) {
-        ScottishBankruptOfficerSearchResult details = new ScottishBankruptOfficerSearchResult();
 
-        details.setEphemeralKey(scottishBankruptOfficerDetailsDataModel.getEphemeralKey());
-        details.setForename1(scottishBankruptOfficerDetailsDataModel.getForename1());
-        details.setForename2(scottishBankruptOfficerDetailsDataModel.getForename2());
-        details.setSurname(scottishBankruptOfficerDetailsDataModel.getSurname());
-        details.setDateOfBirth(scottishBankruptOfficerDetailsDataModel.getDateOfBirth());
-        details.setPostcode(scottishBankruptOfficerDetailsDataModel.getAddressPostcode());
-        details.setAddressLine1(scottishBankruptOfficerDetailsDataModel.getAddressLine1());
-        details.setAddressLine2(scottishBankruptOfficerDetailsDataModel.getAddressLine2());
-        details.setAddressLine3(scottishBankruptOfficerDetailsDataModel.getAddressLine3());
-        details.setCounty(scottishBankruptOfficerDetailsDataModel.getAddressCounty());
-        details.setTown(scottishBankruptOfficerDetailsDataModel.getAddressTown());
-        details.setCaseType(scottishBankruptOfficerDetailsDataModel.getCaseType());
+    public ScottishBankruptOfficerSearchResult convertToSearchResult(ScottishBankruptOfficerDataModel scottishBankruptOfficerDetailsDataModel) {
+        ScottishBankruptOfficerSearchResult searchResult = new ScottishBankruptOfficerSearchResult();
+
+        searchResult.setEphemeralKey(scottishBankruptOfficerDetailsDataModel.getEphemeralKey());
+        searchResult.setForename1(scottishBankruptOfficerDetailsDataModel.getForename1());
+        searchResult.setForename2(scottishBankruptOfficerDetailsDataModel.getForename2());
+        searchResult.setSurname(scottishBankruptOfficerDetailsDataModel.getSurname());
+        searchResult.setDateOfBirth(scottishBankruptOfficerDetailsDataModel.getDateOfBirth());
+        searchResult.setPostcode(scottishBankruptOfficerDetailsDataModel.getAddressPostcode());
+        searchResult.setAddressLine1(scottishBankruptOfficerDetailsDataModel.getAddressLine1());
+        searchResult.setAddressLine2(scottishBankruptOfficerDetailsDataModel.getAddressLine2());
+        searchResult.setAddressLine3(scottishBankruptOfficerDetailsDataModel.getAddressLine3());
+        searchResult.setCounty(scottishBankruptOfficerDetailsDataModel.getAddressCounty());
+        searchResult.setTown(scottishBankruptOfficerDetailsDataModel.getAddressTown());
+        searchResult.setCaseType(scottishBankruptOfficerDetailsDataModel.getCaseType());
 
         LocalDate debtorDischargeDate = scottishBankruptOfficerDetailsDataModel.getDebtorDischargeDate();
 
         if (debtorDischargeDate != null && !END_OF_TIME.isEqual(debtorDischargeDate)) {
-            details.setDebtorDischargeDate(debtorDischargeDate);
+            searchResult.setDebtorDischargeDate(debtorDischargeDate);
         }
 
-        return details;
+        return searchResult;
     }
 
 }
