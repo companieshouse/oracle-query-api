b6d34287cca6afdf18bb385f64c8b1c9b1f72d1f
bbell-companieshouse <bbell@companieshouse.gov.uk>
2023-09-14 16:05:20 +0100

    UAR-1133: Remove code smells

--- src/test/java/uk/gov/ch/service/update/trusts/impl/IndividualTrusteesServiceTest.java
+++ src/test/java/uk/gov/ch/service/update/trusts/impl/IndividualTrusteesServiceTest.java
@@ -18,7 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class IndividualTrusteesServiceTest {
+class IndividualTrusteesServiceTest {
     @InjectMocks
     private IndividualTrusteesServiceImpl individualTrusteesService;
 
