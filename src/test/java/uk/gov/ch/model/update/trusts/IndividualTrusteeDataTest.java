2333c5bae151ea8bb49587a3881870d9fff809b6
bbell-companieshouse <bbell@companieshouse.gov.uk>
2023-09-19 08:57:52 +0100

    UAR-1133: Remove code smell

--- src/test/java/uk/gov/ch/model/update/trusts/IndividualTrusteeDataTest.java
+++ src/test/java/uk/gov/ch/model/update/trusts/IndividualTrusteeDataTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class IndividualTrusteeDataTest {
+class IndividualTrusteeDataTest {
     private static final String JSON_STRING =
             "{"
             + "\"trusteeId\":\"123\","
