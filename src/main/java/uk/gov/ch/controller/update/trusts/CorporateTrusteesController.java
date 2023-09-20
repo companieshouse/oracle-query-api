330f669e0f62265fd509561378d85c18582a0bd9
bbell-companieshouse <bbell@companieshouse.gov.uk>
2023-09-20 16:43:07 +0100

    UAR-1136: Fix controller issue

--- src/main/java/uk/gov/ch/controller/update/trusts/CorporateTrusteesController.java
+++ src/main/java/uk/gov/ch/controller/update/trusts/CorporateTrusteesController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.ch.OracleQueryApplication;
 import uk.gov.ch.exception.TrustDataCountNotFoundException;
 import uk.gov.ch.model.update.trusts.CorporateTrusteeData;
@@ -13,6 +14,7 @@ import uk.gov.ch.service.update.trusts.CorporateTrusteesService;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.LoggerFactory;
 
+@RestController
 public class CorporateTrusteesController {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(
@@ -20,7 +22,7 @@ public class CorporateTrusteesController {
     @Autowired
     private CorporateTrusteesService corporateTrusteesService;
 
-    @GetMapping("overseas-entity/trusts/{trustId}/corporate-trustees")
+    @GetMapping("/overseas-entity/trusts/{trustId}/corporate-trustees")
     public ResponseEntity<List<CorporateTrusteeData>> getCorporateTrusteeData(
             @PathVariable String trustId) {
 
