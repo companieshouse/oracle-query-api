eda963bfb487a11a8e9a29d4dd20a8071d859448
bbell-companieshouse <124162631+bbell-companieshouse@users.noreply.github.com>
2023-09-20 12:06:32 +0100

    UAR-1144: Fix SQL query issue (#175)

--- src/main/java/uk/gov/ch/repository/update/trusts/TrustDetailsRepository.java
+++ src/main/java/uk/gov/ch/repository/update/trusts/TrustDetailsRepository.java
@@ -7,7 +7,7 @@ import uk.gov.ch.model.update.trusts.TrustDetails;
 import java.util.List;
 
 public interface TrustDetailsRepository extends PagingAndSortingRepository<TrustDetails, Long> {
-    @Query(value = "select t.trust_id, t.trust_name, t.creation_date, t.ceased_date, t.unable_to_obtain_all_info_ind "
+    @Query(value = "select distinct t.trust_id, t.trust_name, t.creation_date, t.ceased_date, t.unable_to_obtain_all_info_ind "
             + "from corporate_body cb "
             + "join corporate_body_appointment ca on ca.corporate_body_id = cb.corporate_body_id "
             + "join corbod_appt_trust_link cat on ca.corporate_body_appointment_id = cat.corporate_body_appointment_id "
