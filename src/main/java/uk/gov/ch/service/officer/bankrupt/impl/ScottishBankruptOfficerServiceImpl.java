5cfdcdab6fe9f1f1d437c143a34d2525d5631b55
kdobija1 <kdobija1@companieshouse.gov.uk>
2025-09-15 11:14:55 +0100

    add logging to bankrupt officer search

--- src/main/java/uk/gov/ch/service/officer/bankrupt/impl/ScottishBankruptOfficerServiceImpl.java
+++ src/main/java/uk/gov/ch/service/officer/bankrupt/impl/ScottishBankruptOfficerServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import uk.gov.ch.OracleQueryApplication;
 import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDataModel;
 import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerDetails;
 import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearch;
@@ -14,9 +15,14 @@ import uk.gov.ch.model.officer.bankrupt.ScottishBankruptOfficerSearchResults;
 import uk.gov.ch.repository.officers.ScottishBankruptOfficersRepository;
 import uk.gov.ch.service.officer.bankrupt.BankruptOfficerService;
 import uk.gov.ch.transformers.officer.bankrupt.BankruptOfficersTransformer;
+import uk.gov.companieshouse.logging.Logger;
+import uk.gov.companieshouse.logging.LoggerFactory;
 
 @Service
 public class ScottishBankruptOfficerServiceImpl implements BankruptOfficerService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(
+            OracleQueryApplication.APPLICATION_NAME_SPACE);
 
     @Autowired
     private ScottishBankruptOfficersRepository scottishBankruptOfficersRepository;
@@ -27,6 +33,7 @@ public class ScottishBankruptOfficerServiceImpl implements BankruptOfficerServic
     @Override
     public ScottishBankruptOfficerSearchResults getScottishBankruptOfficers(
             ScottishBankruptOfficerSearch search) {
+        LOGGER.info("Calling service to search scottish bankrupt officers with: " + search.toString());
         Pageable page = PageRequest.of(search.getStartIndex(), search.getItemsPerPage());
 
         ScottishBankruptOfficerSearchFilters filters = search.getFilters();
@@ -40,6 +47,7 @@ public class ScottishBankruptOfficerServiceImpl implements BankruptOfficerServic
 
     public ScottishBankruptOfficerDetails getScottishBankruptOfficer(String ephemeralId) {
 
+        LOGGER.info("Calling service to search for single scottish bankrupt officer with ID: " + ephemeralId);
         Optional<ScottishBankruptOfficerDataModel> officerModel = scottishBankruptOfficersRepository.findById(
                 ephemeralId);
 
