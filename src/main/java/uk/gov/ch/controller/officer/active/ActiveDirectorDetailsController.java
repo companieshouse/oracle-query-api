10503ded3d7bb1769b262cb4c0271cb991838f7b
NeilShadrach <nshadrach@companieshouse.gov.uk>
2026-03-18 10:28:35 +0000

    ASM-1537 Use constructor injection.

--- src/main/java/uk/gov/ch/controller/officer/active/ActiveDirectorDetailsController.java
+++ src/main/java/uk/gov/ch/controller/officer/active/ActiveDirectorDetailsController.java
@@ -1,6 +1,5 @@
 package uk.gov.ch.controller.officer.active;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
