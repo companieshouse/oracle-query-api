95653ae27036d634adb2c7bdca8391aeddacfc0d
NeilShadrach <nshadrach@companieshouse.gov.uk>
2026-03-18 13:28:33 +0000

    ASM-1537 Use constructor injection.

--- src/main/java/uk/gov/ch/service/update/trusts/impl/TrustLinksServiceImpl.java
+++ src/main/java/uk/gov/ch/service/update/trusts/impl/TrustLinksServiceImpl.java
@@ -1,7 +1,6 @@
 package uk.gov.ch.service.update.trusts.impl;
 
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.ch.OracleQueryApplication;
 import uk.gov.ch.exception.TrustDataCountNotFoundException;
@@ -17,8 +16,11 @@ public class TrustLinksServiceImpl implements TrustLinksService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(
             OracleQueryApplication.APPLICATION_NAME_SPACE);
-    @Autowired
     private TrustLinksRepository trustLinksRepository;
+
+    public TrustLinksServiceImpl(TrustLinksRepository trustLinksRepository) {
+        this.trustLinksRepository = trustLinksRepository;
+    }
 
     @Override
     public List<TrustLinkData> getTrustLinkData(String oeNumber)
