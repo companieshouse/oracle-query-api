86a943ecd5f178276a699168987f3785c2dc07e1
NeilShadrach <nshadrach@companieshouse.gov.uk>
2026-03-19 14:41:41 +0000

    ASM-1542 Remove unused import

--- src/test/java/uk/gov/ch/service/register/impl/RegisterLocationServiceImplTest.java
+++ src/test/java/uk/gov/ch/service/register/impl/RegisterLocationServiceImplTest.java
@@ -10,7 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import uk.gov.ch.model.psc.PersonWithSignificantControl;
 import uk.gov.ch.model.register.RegisterLocation;
 import uk.gov.ch.repository.register.RegisterLocationRepository;
 
